0b6c9aa61c7f22920388ae5f3edd142082209ee1
dellsystem <ilostwaldo@gmail.com>
2013-11-22 22:53:08 -0500

    Rename initial_data fixture
    Shouldn't need to be loaded every time syncdb is run.

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,9 @@ from fabric.api import local
 def up():
     local("python manage.py runserver")
 
+def load():
+    local("python manage.py loaddata mileage_data.json")
+
 def dump():
     local("python manage.py dumpdata --format=json --indent=4 howmanymiles"
-        " > howmanymiles/fixtures/initial_data.json")
+        " > howmanymiles/fixtures/mileage_data.json")
