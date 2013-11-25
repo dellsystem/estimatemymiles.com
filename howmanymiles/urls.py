ffe7af25aff2ce0199455e962e3a21d577b11d75
dellsystem <ilostwaldo@gmail.com>
2013-11-24 22:20:03 -0500

    Add about page

--- howmanymiles/urls.py
+++ howmanymiles/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.views.generic import TemplateView
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,5 +10,7 @@ urlpatterns = patterns('',
     # url(r'^howmanymiles/', include('howmanymiles.foo.urls')),
 
     url(r'^$', 'howmanymiles.views.home', name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'),
+        name='about'),
     url(r'^admin/', include(admin.site.urls)),
 )
