c5a9c43309389d29bd864bd0b0e857bc48339227
dellsystem <ilostwaldo@gmail.com>
2014-01-05 01:12:10 -0400

    Temporarily update homepage view
    Currently just shows the airline logos, to make it easier for me to
    see which ones are missing.

--- howmanymiles/views.py
+++ howmanymiles/views.py
@@ -7,8 +7,7 @@ from howmanymiles.models import Airline, Alliance
 
 def home(request):
     context = {
-        'traveling_airlines': Airline.objects.get_traveling(),
-        'all_airlines': Airline.objects.all(),
+        'airlines': Airline.objects.all(),
     }
 
     return render(request, 'home.html', context)
